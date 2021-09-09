b245a5d6fdf8e1ddd71d91bb7ad6ec4695e73263
jfbentsen <jfbentsen@gmail.com>
2021-09-09 22:41:51 +0200

    properly refferenced text and started leapFunc+test

--- MyFirstCs.Tests/UnitTest1.cs
+++ MyFirstCs.Tests/UnitTest1.cs
@@ -6,9 +6,25 @@ namespace MyFirstCs.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
+        public void fourIsModuloszero()
+        { 
+            int yearA = 111;
+            int yearB = 1921;
+            int yearC = 400;
+            int yearD = 844;
+            
+            
+            Assert.Equal(MyFirstCs.Program.isLeapYear(yearA),true);
+            Assert.NotEqual(MyFirstCs.Program.isLeapYear(yearA),true);
 
+            Assert.Equal(MyFirstCs.Program.isLeapYear(yearB), true);
+            Assert.NotEqual(MyFirstCs.Program.isLeapYear(yearB), true);
+
+            Assert.Equal(MyFirstCs.Program.isLeapYear(yearC), true);
+            Assert.NotEqual(MyFirstCs.Program.isLeapYear(yearC), true);
+
+            Assert.Equal(MyFirstCs.Program.isLeapYear(yearD), true);
+            Assert.NotEqual(MyFirstCs.Program.isLeapYear(yearD), true);
         }
     }
 }
--- MyFirstCs/Program.cs
+++ MyFirstCs/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace MyFirstCs
 {
-    class Program
+    public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string val;
+            Console.Write("Enter integer to check for leap year: ");
+            val = Console.ReadLine();
+            int a = Convert.ToInt32(val);
+            if (isLeapYear(a)){
+                Console.Write("yay");
+            } else
+            {
+                Console.Write("nay");
+            }
+        }
+
+        public static bool isLeapYear(int year) {
+            if (year%4==0)
+            {
+                if (year%100==0)
+                {
+                    if (year%400==0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
